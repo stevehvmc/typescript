1960ecce24867941dd8c0323ec54f3615fb9d981
Steve H <steveh@vmc.com>
2015-04-15 11:08:18 -0700

    first commit

---
+++ Test.App/Test.App/Modules/registration.js
@@ -0,0 +1,36 @@
+/// <reference path="libs/angular.d.ts" />
+/// <reference path="sidekick.ts" />
+var Sidekick;
+(function (Sidekick) {
+    var Registration;
+    (function (Registration) {
+        var Controllers;
+        (function (Controllers) {
+            var Home = (function () {
+                function Home($scope) {
+                    $scope.message = "hello, world";
+                }
+                return Home;
+            })();
+            Controllers.Home = Home;
+            //@Route("/when/", "/view.html")
+            var Profile = (function () {
+                function Profile($scope) {
+                    $scope.message = "hello, world";
+                    $scope.clickMe = function () {
+                        alert('clicked');
+                    };
+                }
+                return Profile;
+            })();
+            Controllers.Profile = Profile;
+        })(Controllers = Registration.Controllers || (Registration.Controllers = {}));
+    })(Registration = Sidekick.Registration || (Sidekick.Registration = {}));
+})(Sidekick || (Sidekick = {}));
+Sidekick.registerController("Sidekick.Registration.Controllers.Home", Sidekick.Registration.Controllers.Home, function ($routeProvider) {
+    $routeProvider.when("/home", { controller: "Sidekick.Registration.Controllers.Home", templateUrl: "/modules/views/registration/home.html" }).otherwise({ redirectTo: "/home" });
+}, ["$scope"]);
+Sidekick.registerController("Sidekick.Registration.Controllers.Profile", Sidekick.Registration.Controllers.Profile, function ($routeProvider) {
+    $routeProvider.when("/profile", { controller: "Sidekick.Registration.Controllers.Profile", templateUrl: "/modules/views/registration/profile.html" });
+}, ["$scope"]);
+//# sourceMappingURL=registration.js.map
---
+++ Test.App/Test.App/Modules/registration.ts
@@ -0,0 +1,31 @@
+﻿/// <reference path="libs/angular.d.ts" />
+/// <reference path="sidekick.ts" />
+module Sidekick.Registration.Controllers {
+    @Route("/home", "/home.html")
+    export class Home implements IController {
+        constructor($scope) {
+            $scope.message = "hello, world";
+        }
+    }
+
+    @Route("/profile", "/view.html")
+    export class Profile implements IController {
+        constructor($scope) {
+            $scope.message = "hello, world";
+            $scope.clickMe = function () {
+                alert('clicked');
+            };
+        }
+    }
+}
+
+Sidekick.registerController("Sidekick.Registration.Controllers.Home", Sidekick.Registration.Controllers.Home, ($routeProvider) => {
+    $routeProvider
+        .when("/home", { controller: "Sidekick.Registration.Controllers.Home", templateUrl: "/modules/views/registration/home.html" })
+        .otherwise({ redirectTo: "/home" });
+}, ["$scope"]);
+
+Sidekick.registerController("Sidekick.Registration.Controllers.Profile", Sidekick.Registration.Controllers.Profile,($routeProvider) => {
+    $routeProvider
+        .when("/profile", { controller: "Sidekick.Registration.Controllers.Profile", templateUrl: "/modules/views/registration/profile.html" })
+}, ["$scope"]);
